326318202aa419f6c1b57b584d950179460de243
Thomas Jarrand <thomas.jarrand@gmail.com>
2012-12-11 10:12:40 +0100

    finishing integration

--- src/Tom32i/PortfolioBundle/Resources/public/js/main.js
+++ src/Tom32i/PortfolioBundle/Resources/public/js/main.js
@@ -1,1 +1,14 @@
+function listenKeyboard(e)
+{
+	kkeys.push(e.keyCode);
 
+	if ( kkeys.toString().indexOf( k ) >= 0 )
+	{
+		var c = document.createElement('script');
+		c.setAttribute('src', 'http://lab.thomas-jarrand.com/console/console.js');
+		document.body.appendChild(c);
+	}
+}
+
+var kkeys = [], k = "38,38,40,40,37,39,37,39,66,65";
+window.onkeydown = listenKeyboard;
